dd3c7827716986e0e7bf3601b956f6910abd0ce1
Tirzo01 <tiziano.nicosia01@gmail.com>
2023-01-30 17:12:24 +0100

    30-01-23

---
+++ react-router/src/routes/users/Users.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GET } from "../../http/http";
+import styles from "./styles.module.scss";
+
+export function Users() {
+  const { userID } = useParams();
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    GET(`users/${userID}`).then((res) => {
+      setUser(res);
+    });
+  }, []);
+
+  if (Object.keys(user).length == 0) return <p>Caricamento</p>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <div className={styles.avatar_container}>
+          <img src={user.image} alt="" />
+        </div>
+        <h2>
+          {console.log(user)}
+          {user.firstName} {user.lastName}
+        </h2>
+        {console.log(user)}
+        <p>Address: {user.address.address}</p>
+      </div>
+    </div>
+  );
+}
