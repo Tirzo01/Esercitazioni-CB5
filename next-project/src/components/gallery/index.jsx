6f6d37ea5b7dc946d870d56baff5106a517214df
Tirzo01 <tiziano.nicosia01@gmail.com>
2023-02-02 16:27:22 +0100

    02-02-23

---
+++ next-project/src/components/gallery/index.jsx
@@ -0,0 +1,51 @@
+import { Righteous } from "@next/font/google";
+import Image from "next/image";
+import { useRef } from "react";
+import styles from "./styles.module.scss";
+
+export default function Gallery() {
+  const gallery = useRef(null);
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1280/720/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1280/720/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1280/720/",
+    },
+  ];
+
+  const moveRight = () => {
+    gallery.current.scrollBy({
+      top: 0,
+      left: 1280 + 20,
+      behavior: "smooth",
+    });
+  };
+
+  const moveLeft = () => {
+    gallery.current.scrollBy({
+      top: 0,
+      left: -1280 - 20,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className={styles.main}>
+      <div ref={gallery}>
+        {images.map((image) => (
+          <Image src={image.original} width={1280} height={720} />
+        ))}
+      </div>
+      <button className={styles.prev} onClick={() => moveLeft()}>
+        prev
+      </button>
+      <button className={styles.next} onClick={() => moveRight()}>
+        next
+      </button>
+    </div>
+  );
+}
