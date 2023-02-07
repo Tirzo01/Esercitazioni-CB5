4e2fc0d0f40edc4b3f344a2b80c12c901c94431a
Tirzo01 <tiziano.nicosia01@gmail.com>
2023-02-07 17:21:45 +0100

    07-02-23

---
+++ todolist/src/App.jsx
@@ -0,0 +1,32 @@
+import { useReducer } from "react";
+import "./App.css";
+import { ApplicationCtx, initialState } from "../src/store/state";
+import { TodoList } from "./components/todoList";
+
+function mainReducer(state, action) {
+  switch (action.type) {
+    case "TOGGLE":
+      console.log(action.payload);
+      return {
+        todoList: state.todoList.map((todoitem) => {
+          if (todoitem.id == action.payload) {
+            todoitem.status = !todoitem.status;
+          }
+          return todoitem;
+        }),
+      };
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(mainReducer, initialState);
+  return (
+    <ApplicationCtx.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <TodoList></TodoList>
+      </div>
+    </ApplicationCtx.Provider>
+  );
+}
+
+export default App;
